ae0fd312dc1bb34b7b8eb183800cdfdb05d4f8d8
sajagshrestha <sajagshrestha0852@gmail.com>
2020-09-23 11:07:23 +0545

    FIRST COMMIT

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from "react";
+import styled from "styled-components";
+//global css
+const Main = styled.div`
+    background-color: #1e2025;
+    width: 100vw;
+    height: 100vh;
+    margin: 0;
+    padding: 0;
+`;
+//main App component
+const App = () => {
+    return <Main>Hello world</Main>;
+};
 
 export default App;
